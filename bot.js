6591a6addbfb9460f186f9c417721cfc1d4f5ef6
hadehma <45009026+hadehma@users.noreply.github.com>
2019-07-03 18:23:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "511935022400405505"; // ???? ???????
-var channel = "515821146919403521";//???? ?????
+var server = "595997769999712276"; // ???? ???????
+var channel = "595997770129735782";//???? ?????
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**i love _Hma , i love _Hma , i love _Hma  , i love _Hma   , i love _Hma   , i love _Hma   , i love _Hma   , i love _Hma   , i love _Hma   , i love _Hma   , i love _Hma   , i love _Hma   , i love _Hma   , i love _Hma   , i love _Hma   , i love _Hma   , i love _Hma   , i love _Hma   , **')
     },305);
